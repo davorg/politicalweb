cd29adc9529c85b342bb8c9b0cc6b15afc7fe79c
Dave Cross <dave@dave.org.uk>
2013-02-27 21:18:34 +0000

    Get a basic version running (again)

--- PoliticalWeb/lib/PoliticalWeb.pm
+++ PoliticalWeb/lib/PoliticalWeb.pm
@@ -3,10 +3,13 @@ use Dancer ':syntax';
 use Dancer::Plugin::DBIC;
 use Dancer::Plugin::Cache::CHI;
 use WebService::TWFY::API;
+use Data::Dumper;
 
 my $twfy = WebService::TWFY::API->new({
   key => $ENV{TWFY_KEY}
 }) or die $!;
+my $con_rs = schema->resultset('Constituency');
+my $mp_rs  = schema->resultset('Mp');
 
 our $VERSION = '0.1';
 
@@ -35,23 +38,26 @@ get '/constituency/:constname' => sub {
 };
 
 sub get_const {
-  unless (cache_get "C:$_[0]") {
-    my $ret = $twfy->query( 'getConstituency', {
+  my ($con, $ret);
+  unless ($con = cache_get "C:$_[0]") {
+    $ret = $twfy->query( 'getConstituency', {
       name => $_[0],
       postcode => '',
     });
-  
+
+    debug Dumper $ret;
+
     if ($ret->{is_success}) {
-      cache_set "C:$_[0]", from_json($ret->{results}), 60*60*60;
+      debug "Setting cache key - 'C:$_[0]'";
+      debug 'Setting cache val - ' . Dumper(from_json($ret->{results}));
+      $con = from_json($ret->{results});
+      cache_set "C:$_[0]", $con, 60*60*60;
     }
   }
+
+  debug Dumper $con;
   
-  my $con = cache_get "C:$_[0]";
-  
-  my $con_rs = schema->resultset('Constituency');
-  if (my $con_db = $con_rs->find({ name => $con->{name} })) {
-    $con->{db} = $con_db;
-  }
+  $con->{db} = $con_rs->find_or_create({ name => $con->{name} });
   
   return $con;
 }
@@ -73,16 +79,20 @@ sub get_const_name_from_pc {
 sub get_mp {
   my $constit = shift;
   my $c_name = $constit->{name};
-  
-  unless (cache_get "M:$c_name") {
+
+  my $mp;
+  unless ($mp = cache_get "M:$c_name") {
     my $ret = $twfy->query( 'getMP', {
       constituency => $c_name,
     });
-  
-    my $mp;
+
+    debug "TWFY: $ret";
+
     if ($ret->{is_success}) {
       $mp = from_json($ret->{results});
-    
+
+      debug Dumper $mp;
+
       my $ret = $twfy->query( 'getMPInfo', {
         id => $mp->{person_id},
       });
@@ -95,9 +105,9 @@ sub get_mp {
       cache_set "M:$c_name", $mp, 60*60*60;
     }
   }
-  
-  my $mp = cache_get "M:$c_name";
-    
+
+  debug "TWFY: MP is " . Dumper $mp;
+
   $mp->{db} = $constit->{db}->mp;
 
   return $mp;
